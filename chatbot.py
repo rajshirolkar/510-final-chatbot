1a373ed0a554d22c11918968ad1a59c4a6173f68
Scott Stevson <scott@stevson.com>
2023-03-08 04:39:07 -0800

    updates to the main application; adding requirements.txt

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,39 @@
+"""Flask application with embedded chatbot and analytics"""
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'bdqVBWEsRebA4d@GiXm7'
 
 
 class Chatbot(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', e=e), 404
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = Chatbot()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+
+@app.route('/analytics')
+def analytics():
+    return render_template('analytics.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
