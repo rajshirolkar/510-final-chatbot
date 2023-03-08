0d43e96f62c7a8c09fb57bb0451fe99f445b88eb
Scott Stevson <scott@stevson.com>
2023-03-08 05:12:10 -0800

    adding a base.html template; adding flask-moment

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,14 @@
 """Flask application with embedded chatbot and analytics"""
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 app.config['SECRET_KEY'] = 'bdqVBWEsRebA4d@GiXm7'
 
 
